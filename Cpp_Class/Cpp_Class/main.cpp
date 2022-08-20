3104d285086588d013dc1e2a526a2a4244663119
Hyeong1204 <cho4870@naver.com>
2022-08-20 15:58:44 +0900

    소, 돼지, 닭

--- Cpp_Class/Cpp_Class/main.cpp
+++ Cpp_Class/Cpp_Class/main.cpp
@@ -3,63 +3,154 @@
 
 using namespace std;;
 
-class Student {
-private :
-	string _name;	// 멤버 변수
-	int _age;
-	int _grade;
+// 상속
+// 동물 농장 시뮬레이션
+// 돼지, 소, 닭
 
-public :
-	Student() {
+class Pig {
+private:
+	string _name;
+	float _age;
+	float _weight;
+	float _height;
 
+public:
+	Pig(string name, float age, float weight, float height)
+		:_name(name), _age(age), _weight(weight), _height(height) {}
+
+	void Speak() {
+		cout << _name <<"이 꿀꿀합니다. " << endl;
 	}
-	Student(string name, int age, int grade)		// 인자를 받는 생성자
-		:_name(name), _age(age), _grade(grade) {}
 
-	void init(string name, int age, int grade) {
-		_name = name;
-		_age = age;
-		_grade = grade;
+	void Eat() {
+		cout << _name <<"이 먹습니다. " << endl;
+	}
+
+	void Run() {
+		cout << _name << "이 뜁니다. " << endl;
 	}
 
 	void info() {
 		cout << "이름 : " << _name << endl;
 		cout << "나이 : " << _age << endl;
-		cout << "학년 : " << _grade << endl;
+		cout << "몸무게 : " << _weight << endl;
+		cout << "키 : " << _height << endl;
 	}
 };
 
-int global = 100;	// 외부변수, 전역변수
+class Cow {
+private:
+	string _name;
+	float _age;
+	float _weight;
+	float _height;
 
-int SetScore(int value) {
-	static int score = 0;	// 로컬변수(지역변수), 정적변수
-	score += value;
-	return score;
-}
+public:
+	Cow(string name, float age, float weight, float height)
+		:_name(name), _age(age), _weight(weight), _height(height) {}
 
-// 동적객체
-// 변수의 종류
-// 접근범위, 존속기간
+	void Speak() {
+		cout << _name << "이 음매합니다. " << endl;
+	}
+
+	void Eat() {
+		cout << _name << "이 먹습니다. " << endl;
+	}
+
+	void Run() {
+		cout << _name << "이 뜁니다. " << endl;
+	}
+
+	void info() {
+		cout << "이름 : " << _name << endl;
+		cout << "나이 : " << _age << endl;
+		cout << "몸무게 : " << _weight << endl;
+		cout << "키 : " << _height << endl;
+	}
+};
+
+class Chicken {
+private :
+	string _name;
+	float _age;
+	float _weight;
+	float _height;
+	bool _fly;
+
+	void Fly() {
+		cout << _name << "이 납니다. " << endl;
+	}
+
+public:
+	Chicken(string name, float age, float weight, float height, bool fly)
+		:_name(name), _age(age), _weight(weight), _height(height), _fly(fly) {}
+
+	void Speak() {
+		cout << _name << "이 꼬끼오합니다. " << endl;
+	}
+
+	void Eat() {
+		cout << _name << "이 먹습니다. " << endl;
+	}
+
+	void Run() {
+		if (_fly)
+		{
+			Fly();
+		}
+		else
+		cout << _name << "이 뜁니다. " << endl;
+	}
+
+	void info() {
+		cout << "이름 : " << _name << endl;
+		cout << "나이 : " << _age << endl;
+		cout << "몸무게 : " << _weight << endl;
+		cout << "키 : " << _height << endl;
+		if (_fly)
+		{
+			cout << "종류 : 나는 닭" << endl;
+		}
+		else
+			cout << "종류 : 못 나는 닭" << endl;
+	}
+};
+
 int main(void) {
-	
-	Student* pa = new Student("monster", 10, 3);	// 동적객체 할당
+	//Student* pa = new Student("monster", 10, 3);	// 동적객체 할당
+	//pa->info();	// 주소값으로 클래스의 멤버에 접근할때는 화살표 연산자를 사용합니다.
+	//delete pa;	// 동적객체 반환
+	//Student *parray = new Student[5];	// 동적객체 배열을 만들려면 기본 생성자를 만들어야함
+	//for (int i = 0; i < 5; i++) {
+	//	parray[i].init("monster", i, i);
+	//}
+	//for (int i = 0; i < 5; i++) {
+	//	parray[i].info();
+	//}
+	//delete[] parray;	// 동적객체 배열 할당 해제
 
-	pa->info();	// 주소값으로 클래스의 멤버에 접근할때는 화살표 연산자를 사용합니다.
+	Cow cow("소", 2.2f, 200.0f, 1.5f);
+	Pig pig("돼지", 2.5f, 150.0f, 1.3f);
+	Chicken flyChicken("나는닭", 0.5f, 2.5f, 0.3f, true);
+	Chicken notFlyChicken("못나는닭", 0.5f, 2.5f, 0.3f, false);
 
-	
+	cow.Speak();
+	cow.Run();
 
-	delete pa;	// 동적객체 반환
+	cout << endl;
 
-	Student *parray = new Student[5];	// 동적객체 배열을 만들려면 기본 생성자를 만들어야함
+	pig.Speak();
+	pig.Run();
 
-	for (int i = 0; i < 5; i++) {
-		parray[i].init("monster", i, i);
-	}
-	for (int i = 0; i < 5; i++) {
-		parray[i].info();
-	}
+	cout << endl;
 
-	delete[] parray;	// 동적객체 배열 할당 해제
+	flyChicken.Speak();
+	flyChicken.Run();
+
+	cout << endl;
+
+	notFlyChicken.Speak();
+	notFlyChicken.Run();
 
 	return 0;
 }
@@ -92,7 +183,37 @@ void Test_count()
 	delete[] parray;	// 동적배열 반환
 }
 
+int SetScore(int value) {
+	static int score = 0;	// 로컬변수(지역변수), 정적변수
+	score += value;
+	return score;
+}
 
+//class Student {
+//private :
+//	string _name;	// 멤버 변수
+//	int _age;
+//	int _grade;
+//
+//public :
+//	Student() {
+//
+//	}
+//	Student(string name, int age, int grade)		// 인자를 받는 생성자
+//		:_name(name), _age(age), _grade(grade) {}
+//
+//	void init(string name, int age, int grade) {
+//		_name = name;
+//		_age = age;
+//		_grade = grade;
+//	}
+//
+//	void info() {
+//		cout << "이름 : " << _name << endl;
+//		cout << "나이 : " << _age << endl;
+//		cout << "학년 : " << _grade << endl;
+//	}
+//};
 
 
 
@@ -151,4 +272,4 @@ void Test_count()
 //	cout << c << " + " << d << " = " << fret << endl;
 //
 //	return 0;
-//}
+//};
